b58ba69faeab8571bea41e425ff402248c86e22c
smoreno10 <morenosanti@hotmail.com>
2022-06-29 19:32:48 -0300

    Lista de alumnos

--- src/app/contenido/contenido.component.ts
+++ src/app/contenido/contenido.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { alumnos } from '../data/alumnos'
 
 @Component({
   selector: 'app-contenido',
   templateUrl: './contenido.component.html',
   styleUrls: ['./contenido.component.css']
 })
+
 export class ContenidoComponent implements OnInit {
+
+  public mensaje: string = "Bienvenid@ a mi primera app en angular!"
+  
+  alumnos = alumnos;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
---
+++ src/app/data/alumnos.ts
@@ -0,0 +1,8 @@
+export const alumnos = [
+    { id: 1, nombre: "Ezequiel Lavalle", nota: 6 },
+    { id: 2, nombre: "Tomas Moreno", nota: 2 },
+    { id: 3, nombre: "Solange Cubillas", nota: 7 },
+    { id: 4, nombre: "Julieta Fernandez", nota: 4 },
+    { id: 5, nombre: "Martina Toledo", nota: 9 }
+]
+
